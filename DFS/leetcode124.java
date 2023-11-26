49b9c8596f5419511746f3adf3042fc4bdf99f70
Jinglun Jiang <jinglunjiang@gmail.com>
2023-11-26 13:13:10 -0600

    Binary Tree Maximum Path Sum

---
+++ DFS/leetcode124.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max;
+    public int maxPathSum(TreeNode root) {
+        max = Integer.MIN_VALUE;
+        maxSum(root);
+        return max;
+    }
+
+    public int maxSum(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        int left = Math.max(0, maxSum(root.left));
+        int right = Math.max(0, maxSum(root.right));
+        max = Math.max(max, root.val + left + right);
+        return Math.max(left, right) + root.val;
+    }
+}
