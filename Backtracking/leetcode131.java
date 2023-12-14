e7f6c2e01bd646f2045a42b2bc8279c88b4ced22
Jinglun Jiang <jinglunjiang@gmail.com>
2023-12-14 11:01:34 -0600

    Palindrome Partitioning

---
+++ Backtracking/leetcode131.java
@@ -0,0 +1,35 @@
+class Solution {
+    List<List<String>> result = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+        backtracking(s, 0, new ArrayList<>());
+        return result;
+    }
+
+    private void backtracking(String s, int index, List<String> list){
+        if(index >= s.length()){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = index; i < s.length(); i++){
+            if (isPalindrome(s.substring(index, i + 1))){
+                list.add(s.substring(index, i + 1));
+                backtracking(s, i + 1, list);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public boolean isPalindrome(String s){
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j){
+            if (s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
