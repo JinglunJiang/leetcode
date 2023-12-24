11d1a27139f8cc43a7cb6882cbe36f8e9d4ad8e5
Jinglun Jiang <jinglunjiang@gmail.com>
2023-12-24 11:53:34 -0600

    Number of Islands

---
+++ DFS/leetcode200(2).java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                if (grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(char[][] grid, int i, int j){
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        for (int[] direction : directions){
+            int row = i + direction[0];
+            int column = j + direction[1];
+            if (row >= 0 && row < grid.length && column >= 0 && column < grid[0].length && grid[row][column] == '1'){
+                grid[row][column] = '0';
+                dfs(grid, row, column);
+            }
+        }
+    }
+}
