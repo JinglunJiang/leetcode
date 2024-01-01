645947b5af70b1aafaf7dc3231ff2787625d955a
Jinglun Jiang <jinglunjiang@gmail.com>
2024-01-01 15:10:23 -0600

    Minimum Number of Arrows to Burst Ballon

---
+++ Greedy/leetcode452.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int sum = 1;
+        for (int i = 1; i < points.length; i++){
+            if (points[i][0] <= points[i - 1][1]){
+                points[i][1] = Math.min(points[i][1], points[i - 1][1]);
+            }
+            else{
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
