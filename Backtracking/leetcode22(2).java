f6deca2864d6cbd4560ee7f9f0ef8422a7803053
Jinglun Jiang <jinglunjiang@gmail.com>
2024-01-22 14:25:15 -0600

    Generate Parentheses

---
+++ Backtracking/leetcode22(2).java
@@ -0,0 +1,28 @@
+class Solution {
+    List<String> result = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        if (n == 0){
+            return result;
+        }
+        backtracking(n, new StringBuilder(), 0);
+        return result;
+    }
+
+    public void backtracking(int n, StringBuilder str, int count){
+        if (str.length() == n * 2){
+            if (count == 0){
+                String string = str.toString();
+                result.add(string);
+            }
+            return;
+        }
+        if (count > 0){
+            str.append(')');
+            backtracking(n, str, count - 1);
+            str.deleteCharAt(str.length() - 1);
+        }
+        str.append('(');
+        backtracking(n, str, count + 1);
+        str.deleteCharAt(str.length() - 1);
+    }
+}
