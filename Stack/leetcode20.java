13562eebffd3017d869c991524b711704fe96168
Jinglun Jiang <jinglunjiang@gmail.com>
2024-02-01 09:53:44 -0600

    Valid Parentheses

---
+++ Stack/leetcode20.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            if (c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }
+            else{
+                if (c == ')'){
+                    if (stack.isEmpty() || stack.peek() != '('){
+                        return false;
+                    }
+                    else{
+                        stack.pop();
+                    }
+                }
+                if (c == ']'){
+                    if (stack.isEmpty() || stack.peek() != '['){
+                        return false;
+                    }
+                    else{
+                        stack.pop();
+                    }
+                }
+                if (c == '}'){
+                    if (stack.isEmpty() || stack.peek() != '{'){
+                        return false;
+                    }
+                    else{
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
