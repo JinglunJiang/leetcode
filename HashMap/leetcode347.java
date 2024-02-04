50a6d89d045fce5525b24ac5c50009293d521140
Jinglun Jiang <jinglunjiang@gmail.com>
2024-02-03 23:24:15 -0600

    Top k Frequent Elements

---
+++ HashMap/leetcode347.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> Integer.compare(map.get(b), map.get(a)));
+
+        for (int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : map.keySet()){
+            queue.offer(num);
+        }
+
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; i++){
+            result[i] = queue.poll();
+        }
+        
+        return result;
+    }
+}
