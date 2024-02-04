5268cb53e01e9e511d16449d759ffdddfeddec5f
Jinglun Jiang <jinglunjiang@gmail.com>
2024-02-04 11:44:37 -0600

    Container With Most Water

---
+++ Two Pointers/leetcode11(2).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int max = 0;
+        while (i < j){
+            int current = Math.min(height[i], height[j]) * (j - i);
+            max = Math.max(current, max);
+            if (height[i] < height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return max;
+    }
+}
