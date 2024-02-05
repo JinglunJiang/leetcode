2d224697799ce40b7831248101c0b32d07a54fd7
Jinglun Jiang <jinglunjiang@gmail.com>
2024-02-04 20:37:56 -0600

    Merge k Sorted Lists

---
+++ Linked List/leetcode23.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (ListNode head : lists){
+            ListNode current = head;
+            while (head != null){
+                queue.offer(head.val);
+                head = head.next;
+            }
+        }
+        ListNode head = new ListNode();
+        ListNode current = head;
+        while (!queue.isEmpty()){
+            current.next = new ListNode(queue.poll());
+            current = current.next;
+        }
+        return head.next;
+    }
+}
