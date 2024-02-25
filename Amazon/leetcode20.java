387a7b294cb28b3061a387704f031b4537ae9d49
Jinglun Jiang <jinglunjiang@gmail.com>
2024-02-24 22:41:03 -0600

    Valid Parentheses

---
+++ Amazon/leetcode20.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            if (c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }
+            else if (c == ')'){
+                if (stack.isEmpty() || stack.pop() != '('){
+                    return false;
+                }
+            }
+            else if (c == ']'){
+                if (stack.isEmpty() || stack.pop() != '['){
+                    return false;
+                }
+            }
+            else if (c == '}'){
+                if (stack.isEmpty() || stack.pop() != '{'){
+                    return false;
+                }
+            }
+        }
+        if (!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
