5bf9e0e075f3a98026e5095b8a3c759f3157e7b2
Jinglun Jiang <jinglunjiang@gmail.com>
2024-03-02 12:01:54 -0600

    Coin Change

---
+++ Grind75/leetcode322.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        if (amount == 0){
+            return 0;
+        }
+        for (int i = 1; i <= amount; i++){
+            for (int coin : coins){
+                if (i >= coin && dp[i - coin] != amount + 1){
+                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+                }
+            }
+        }
+        if (dp[amount] == amount + 1){
+            return -1;
+        }
+        return dp[amount];
+    }
+}
