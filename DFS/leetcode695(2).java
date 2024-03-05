3516915dc3b7d2d31488a61a4da406c811f4d763
Jinglun Jiang <jinglunjiang@gmail.com>
2024-03-05 15:46:26 -0600

    Max Area of Island

---
+++ DFS/leetcode695(2).java
@@ -0,0 +1,29 @@
+class Solution {
+    int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+    int max = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                if (grid[i][j] == 1){
+                    int size = dfs(grid, i, j);
+                    max = Math.max(size, max);
+                }
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j){
+        grid[i][j] = 0;
+        int size = 1;
+        for (int[] direction : directions){
+            int row = i + direction[0];
+            int column = j + direction[1];
+            if (row >= 0 && column >= 0 && row < grid.length && column < grid[0].length && grid[row][column] == 1){
+                size += dfs(grid, row, column);
+            }
+        }
+        return size;
+    }
+}
