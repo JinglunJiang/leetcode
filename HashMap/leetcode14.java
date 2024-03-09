ca18e8a99b07fecd6b1058fb9baaa5985e649dbb
Jinglun Jiang <jinglunjiang@gmail.com>
2024-03-09 15:50:37 -0600

    Longest Common Prefix

---
+++ HashMap/leetcode14.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(strs);
+        String start = strs[0];
+        String last = strs[strs.length - 1];
+
+        for (int i = 0; i < Math.min(start.length(), last.length()); i++){
+            if (start.charAt(i) != last.charAt(i)){
+                return sb.toString();
+            }
+            sb.append(start.charAt(i));
+        }
+        return sb.toString();
+    }
+}
