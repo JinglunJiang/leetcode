d63b4adb69ac9b621044d6bb7c82ed390afde194
Jinglun Jiang <jinglunjiang@gmail.com>
2024-03-19 10:09:24 -0500

    Valid Perfect Square

---
+++ Two Pointers/leetcode367.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int i = 1;
+        int j = num;
+        while (i <= j){
+            long mid = i + (j - i) / 2;
+            if (mid * mid == num){
+                return true;
+            }
+            else if (mid * mid < num){
+                i = (int) mid + 1;
+            }
+            else{
+                j = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+}
