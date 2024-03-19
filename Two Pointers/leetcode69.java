1af7c5e4667ed6e932eb48ae9c2a349df81a21a7
Jinglun Jiang <jinglunjiang@gmail.com>
2024-03-19 10:14:48 -0500

    Sqrt(x)

---
+++ Two Pointers/leetcode69.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int i = 1;
+        int j = x;
+        while (i <= j){
+            long mid = i + (j - i) / 2;
+            if (mid * mid > x){
+                j = (int) mid - 1;
+            }
+            else if (mid * mid < x){
+                i = (int) mid + 1;
+            }
+            else {
+                return (int) mid;
+            }
+        }
+        return j;
+    }
+}
