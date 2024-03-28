93182482e7e44189aa3d4d48013591224d31f492
Jinglun Jiang <jinglunjiang@gmail.com>
2024-03-27 19:23:55 -0500

    Sort Colors

---
+++ Two Pointers/leetcode75(2).java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int k = nums.length - 1;
+        while (j <= k){
+            if (nums[j] == 0){
+                nums[j] = nums[i];
+                nums[i] = 0;
+                i++;
+                j++;
+            }
+            else if (nums[j] == 2){
+                nums[j] = nums[k];
+                nums[k] = 2;
+                k--;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+}
