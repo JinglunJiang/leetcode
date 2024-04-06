740b8d3599c928f6373df72e5ef31254c9aeb96f
Noella <noella@Noellas-Laptop.local>
2024-04-05 21:51:23 -0500

    Add Two Numbers

--- Linked List/leetcode2.java
+++ Linked List/leetcode2.java
@@ -1,11 +1,11 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
@@ -13,13 +13,13 @@ class Solution {
         ListNode dummy = new ListNode();
         ListNode current = dummy;
         int carry = 0;
-        while (l1 != null || l2 != null){
+        while (l1 != null || l2 != null) {
             int value = carry;
-            if (l1 != null){
+            if (l1 != null) {
                 value += l1.val;
                 l1 = l1.next;
             }
-            if (l2 != null){
+            if (l2 != null) {
                 value += l2.val;
                 l2 = l2.next;
             }
@@ -27,7 +27,7 @@ class Solution {
             carry = value / 10;
             current = current.next;
         }
-        if (carry != 0){
+        if (carry != 0) {
             current.next = new ListNode(1);
         }
         return dummy.next;
